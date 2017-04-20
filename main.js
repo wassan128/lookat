42a185fdfb520644692cb262dde6b906b5e0b411
wassan128 <wassan.taiko.master@gmail.com>
2017-04-20 23:28:03 +0900

    main

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+require("./app");
+const electron = require("electron");
+
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win = null;
+app.on("ready", () => {
+	win = new BrowserWindow({
+		title: "るっく＠",
+		width: 800,
+		height: 500,
+		transparent: true,
+		frame: false,
+		webPreferences: {nodeIntegration: false}
+	});
+	win.loadURL("http://localhost:8000/");
+
+	win.on("closed", () => {
+		win = null;
+	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
